957f3eb6123df2a2dc77b982beed41a65b04b131
manideep <manideep0411@gmail.com>
2023-11-08 16:53:58 +0530

    checking

--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -25,6 +25,7 @@ public class QuestionService {
         return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 
+
     public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
         try {
             return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
