725e9de54e2f6b95aeae57b2df48019666e6fa8b
manideep <manideep0411@gmail.com>
2023-11-14 16:00:46 +0530

    cch

--- src/main/java/com/example/demo/model/Question.java
+++ src/main/java/com/example/demo/model/Question.java
@@ -11,13 +11,9 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String questionTitle;
-    @Column(name = "\"option 1\"")
     private String option1;
-    @Column(name = "\"option 2\"")
     private String option2;
-    @Column(name = "\"option 3\"")
     private String option3;
-    @Column(name = "\"option 4\"")
     private String option4;
     private String answer;
     private String difficultyLevel;
